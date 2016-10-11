6982680302541b9f5d0515bab2d6a5443c58be2e
wbazant <wbazant@ebi.ac.uk>
2016-10-11 11:13:03 +0100

    Fix file format. If user is me, put the file in the right place

--- index.sh
+++ index.sh
@@ -31,7 +31,15 @@ echo "Generating anatomical_systems.txt ..."
 ./join_files.py
 if [ $(hostname) -eq "wbazant-ml"]
 then
-  cp ./out/anatomical_systems.txt ~/ATLAS3.TEST/integration-test-data/ontology
+  cp ./out/anatomical_systems.txt $SRC/resources
+  pushd .
+  cd $SRC/resources
+  if [$(git diff --name-only --cached | wc -l ) != 0 ] then
+    echo "Dirty worktree, commit anatomical_systems.txt by yourself: " ; git diff --name-only --cached
+  else
+    git commit $SRC/resources/anatomical_systems.txt -m "Anatomical systems file"
+  fi
+  popd
 else
   echo "You can update the anatomical_systems.txt now"
 fi
--- join_files.py
+++ join_files.py
@@ -1,10 +1,10 @@
 #! /usr/bin/python
 outfile = open('./out/anatomical_systems.txt', 'w')
-outfile.write('system id | system name | tissue id | tissue name \n')
+outfile.write('#system id \t system name \t tissue id \t tissue name \n')
 for system in open('./out/all_systems', 'r'):
     system_id = system.split('|')[0]
     try:
         for line in open('./out/%s'%system_id, 'r'):
-            outfile.write("%s|%s"%(system.replace('\n', ''), line))
+            outfile.write(("%s|%s"%(system.replace('\n', ''), line)).replace('|','\t'))
     except BaseException:
         print("No data for: %s"%system_id)
